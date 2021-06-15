79111a2233e013ad9913c7de2013271d57976690
codesparsh <snowmansparsh4@gmail.com>
2021-06-15 13:32:25 +0530

    element made editable patch-2

--- frontend/src/components/CreateElement.js
+++ frontend/src/components/CreateElement.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 
 function CreateElement() {
-    const drag_start = (e)=>{
-        // console.log(e.target);
-        e.dataTransfer.setData("text", e.target.id);
+    const [textElement,setTextElement] = useState([]);
+    const [elementId,setElementId] = useState(0);
 
+    const drag_start = (e)=>{
+        e.dataTransfer.setData("text", e.target.id);
     }
 
     const drop = (e)=>{
@@ -19,17 +20,50 @@ function CreateElement() {
 
     const drag_over = (e)=>{
         e.preventDefault();
+        return false;
     }
 
+    const element = (id)=>{
+        var parah = document.createElement('div');
+        parah.id = id;
+        parah.innerHTML = "hello i am sparsh";
+        parah.style.position = 'absolute';
+        parah.draggable = true;
+        parah.addEventListener('dragstart',(e)=>{
+            drag_start(e)
+        })
+        parah.addEventListener('dblclick',(e)=>{
+            e.target.draggable = false;
+            e.target.contentEditable = true
+        })
+        parah.addEventListener('focusout',(e)=>{
+            e.target.contentEditable = false;
+            e.target.draggable = true;
+        }) 
+        parah.addEventListener('mouseover',(e)=>{
+            e.target.style.outline = '2px solid -webkit-focus-ring-color';
+        })
+
+        parah.addEventListener('mouseleave',(e)=>{
+            e.target.style.outline = 'none';
+        })
+        return parah;
+    }
+
+    const create = (e)=>{
+        var parent = e.target.parentNode;
+        var newId = 'drag'+elementId; 
+        console.log(typeof(newId));
+        setElementId(1+elementId);
+        var ele = element(newId);
+        parent.appendChild(ele);
+    }
+
+
+    
     return (
         <div className="body" onDragOver={(e)=>{drag_over(e)}} onDrop={(e)=>drop(e)}>
-            <div className="heading">
-                {/* <button class÷>drag</button> */}
-                <div id="drag2" draggable onDragStart={(e)=>drag_start(e)}>
-                    <p></p>
-                </div>
-              
-            </div>
+            <button onClick={(e)=>create(e)}>Create Element</button>
         </div>
     )
 }
